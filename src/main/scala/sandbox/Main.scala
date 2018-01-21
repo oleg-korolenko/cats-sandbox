3361de1be5c8dfd00263573a48949eacd945a836
olegk <oleg.korolenko@gmail.com>
2018-01-21 12:37:32 +0100

    init commit

---
+++ src/main/scala/sandbox/Main.scala
@@ -0,0 +1,9 @@
+package sandbox
+
+import cats.instances.string._
+import cats.syntax.semigroup._
+
+object Main extends App {
+  println("Hello " |+| "Cats!")
+
+}
---
+++ src/main/scala/sandbox/printable/Printable.scala
@@ -0,0 +1,30 @@
+package sandbox.printable
+
+/**
+  * Created by oleg on 21/01/2018.
+  */
+object Printable {
+
+  import PrintableInstances._
+
+  def format[A](value: A)(implicit printable: Printable[A]): String = {
+    printable.format(value)
+  }
+
+  def print[A](value: A)(implicit printable: Printable[A]): String = {
+    print(format(value))
+  }
+}
+
+trait Printable[A] {
+  def format(value: A): String
+}
+
+object PrintableInstances {
+  implicit val stringPrintable: Printable[String] = new Printable[String] {
+    override def format(value: String): String = value
+  }
+  implicit val intPrintable: Printable[Int] = new Printable[Int] {
+    override def format(value: Int): String = value.toString
+  }
+}
